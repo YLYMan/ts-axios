2108ff0dc52e1ee09da8c8b7f463f14f0192a814
yanlingyun <1278259092@qq.com>
2022-06-16 13:28:24 +0800

    feat: 函数重载

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -8,18 +8,25 @@ axios({
   }
 })
 
-axios.request({
-  url: '/extend/post',
+// axios.request({
+//   url: '/extend/post',
+//   method: 'post',
+//   data: {
+//     msg: 'hii'
+//   }
+// })
+
+// axios.get('/extend/get')
+// axios.options('/extend/options')
+// axios.delete('/extend/delete')
+// axios.head('/extend/head')
+// axios.post('/extend/post', { msg: '123' })
+// axios.put('/extend/put', { msg: '123' })
+// axios.patch('/extend/patch', { msg: '123' })
+
+axios('/extend/post', {
   method: 'post',
   data: {
-    msg: 'hii'
+    msg: '444444'
   }
 })
-
-axios.get('/extend/get')
-axios.options('/extend/options')
-axios.delete('/extend/delete')
-axios.head('/extend/head')
-axios.post('/extend/post', { msg: '123' })
-axios.put('/extend/put', { msg: '123' })
-axios.patch('/extend/patch', { msg: '123' })
--- src/types/index.ts
+++ src/types/index.ts
@@ -56,5 +56,6 @@ export interface Axios {
 }
 
 export interface AxiosInstance extends Axios {
-	(config?: AxiosRequestConfig): AxiosPromise
+	(config: AxiosRequestConfig): AxiosPromise
+	(url: string, config?: AxiosRequestConfig): AxiosPromise
 }
